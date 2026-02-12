d659203975d93162becacd585c97e9f7feea4dc2
Purnendu Shukla(Harsh) <harshshukla2025.hs@outlook.com>
2026-02-12 15:08:12 +0000

    3rd_task

---
+++ 4_event_listners.vue
@@ -0,0 +1,40 @@
+<!--
+
+Dataflow: Data -> Methods -> Trigger (v-on/ @) -> Update Data -> Update DOM
+
+In this example, we have a data property called `count` that is initialized to 0. We have three methods: `increment`, `decrement`, and `square`, which modify the value of `count`.
+
+In the template, we display the current value of `count` and have three buttons that trigger the respective methods when clicked. When a button is clicked, the corresponding method is called, which updates the `count` data property. This change in data automatically updates the DOM to reflect the new value of `count.
+
+-->
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  
+  methods: {
+    increment() {
+      this.count++;
+    },
+    
+    decrement() {
+      this.count--; // no need to assign the result back to count when using increment/decrement operators
+    },
+    
+    square() {
+      this.count = this.count ** 2; // need to assign the result back to count to update it
+    }
+  }
+}
+</script>
+
+<template>
+  <p>Count is: {{ count }}</p>
+  <button @click="increment">Add</button><br><br>
+  <button @click="decrement">Subract</button><br><br>
+  <button @click="square">Square</button>
+</template>
