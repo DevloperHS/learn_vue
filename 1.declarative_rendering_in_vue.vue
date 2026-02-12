b914a3ae06a14f28a7c3f4731ebd179d871f755c
Purnendu Shukla(Harsh) <harshshukla2025.hs@outlook.com>
2026-02-12 13:01:52 +0000

    1st_task

---
+++ 1.declarative_rendering_in_vue.vue
@@ -0,0 +1,17 @@
+# sol for https://vuejs.org/tutorial/#step-2
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Harsh Shukla"
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>Hello, I am {{ message}}</h2>
+  <em>My Name In Reverse!</em>
+  <strong><p>{{ message.split(' ').reverse().join(' ') }}</p></strong>
+</template>
