f0e004c422d1fc651832087b0ac5992d343d2d3a
Purnendu Shukla(Harsh) <harshshukla2025.hs@outlook.com>
2026-02-12 15:37:43 +0000

    5th_task

---
+++ 5_form_binding.vue
@@ -0,0 +1,38 @@
+# sol for https://vuejs.org/tutorial/#step-5
+
+<!--
+
+Data Flow: Data -> Methods -> Trigger (v-model) -> Update Data -> Update DOM
+
+In this example, we have a data property called `msg` that is initialized to an empty string. We use the `v-model` directive on the input element to create a two-way binding between the input's value and the `msg` data property.
+
+When the user types into the input field, the `v-model` directive automatically updates the `msg` data property with the current value of the input. This change in data then updates the DOM to display the new value of `msg` in the paragraph element below.
+
+
+-->
+
+<script>
+export default {
+  data() {
+    return {
+      text: '',
+      msg:''
+    }
+  },
+  methods: {
+    onInput(e) {
+      this.text = e.target.value
+    },
+    
+    // onInput2(e) {
+    //   this.msg = e.target.value
+    // }
+  }
+}
+</script>
+
+<template>
+<!--<input :value="text" @input="onInput2" placeholder="Type here">-->
+  <input v-model="msg" placeholder="Type here">
+  <p>{{ msg }}</p>
+</template>
